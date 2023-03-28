5b3f0c6031e6c748afabdd91e6babcf3b39ff138
3340165724 <112962918+3340165724@users.noreply.github.com>
2023-03-28 20:14:28 +0800

    _

---
+++ python/pythonProject/json/json_keywords.md
@@ -0,0 +1,11 @@
+## 处理JSON数据
+### JSON编码 (了解)
+>
+> 将 Python 对象编码为 JSON 字符串
+> >使用 json.dumps() 方法
+> 
+
+
+### JSON 解码
+> 将 JSON 字符串解码为 Python 对象
+> >使用 json.loads() 方法
--- python/pythonProject/json/json_keywords.py
+++ python/pythonProject/json/json_keywords.py
@@ -1,5 +1,17 @@
 import json
 
+print("-------------------------JSON 编码(将 Python 对象编码为 JSON 字符串)------------------------------------------")
+list = [4, 6, 13, 10, 12]
+jsonStr = json.dumps(list)
+print("转换后类型为%s，值为%s" % (type(jsonStr), jsonStr))
+dict = {"name": "张三", "age": "20", "address": "USA"}
+jsonStr2 = json.dumps(dict)
+print("转换后类型为%s，值为 %s" % (type(jsonStr2), jsonStr2))
+print()
+print()
+
+
+print("--------------------------JSON 解码(将 JSON 字符串解码为 Python 对象)-------------------------------------------")
 jsonData= '{"name":"张三","age":"20","address":"USA"}'
 obj = json.loads(jsonData)
 for item in obj:
@@ -11,6 +23,7 @@ for item in obj:
 print("-----------------------------------------")
 
 ages = []
+list_value = []
 jsonData1= '''
             [
             {"name":"张三","age":"20","address":"USA"},
@@ -18,16 +31,14 @@ jsonData1= '''
             ]
             '''
 obj1 = json.loads(jsonData1)
-list_value = []
-print("---------------------------------------")
 for items in obj1:
     # items["age"]:获取到key为age的所有value值
     ages.append(items["age"])
     for key in items:
         # 添加所有value值到列表
         list_value.append(items[key])
-        print("key", key)
-        print("key:value", items)
+        # print("key", key)
+        # print("key:value", items)
 print("-----------------------------------------")
-print(list_value)
-print(ages)
+print("全部输出", list_value)
+print("只取age的value值", ages)
