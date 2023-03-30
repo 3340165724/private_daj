c2db3f6672cd1ab681bf8f64dafe0edb7f4eed13
3340165724 <3340165724@qq.com>
2023-03-30 09:34:57 +0800

    _

--- python/pythonProject/data processing/wordcount.py
+++ python/pythonProject/data processing/wordcount.py
@@ -1,21 +1,28 @@
-import csv
+import json
+import os
 
 # 读取数据
-file = csv.reader(open("../Resource/address.csv", 'r'))
+import re
+
+file = open("../Resource/address.csv", encoding="UTF-8").read()
 
 # 创建空列表，用于存储处理后的单个数据
 datas = ""
 for line in file:
     # print(line)
     for item in line:
-        datas += item + " "
-print(datas)
+        datas += item
+print("datas:", datas)
+
+# 使用正则表达式替换非字母数字字符为空格
+datas = re.sub(r'[^\w\s]', ' ', datas)
 
 # 将所有字母转换为小写以便进行不区分大小写的统计
 datas = datas.lower()
 
 # 数据切片
-words = datas.split(" ")
+words = datas.split()
+print("words", words)
 
 # 使用Python字典来存储每个单词的出现次数
 word_count = {}
@@ -29,7 +36,23 @@ print("word_count:", word_count)
 
 
 # 打印结果
+max_count = 0
+max_word = ""
 # word_count字典的items()方法来遍历所有的键值对
 for word, count in word_count.items():
-    print(word, count)
+    # 频率出现最多1
+    if count > max_count:
+        max_count = count
+        max_word = word
+print(max_word, max_count)
+
+# 频率出现最多2
+for item_json in word_count:
+    # print("%s=>%s" % (item_json, word_count[item_json]), end="\n")
+    if max_count < word_count[item_json]:
+        max_count = word_count[item_json]
+        max_word = item_json
+print(max_word, max_count)
+    # print("item:", item_json)
+    # print(word_count[item_json])
 
