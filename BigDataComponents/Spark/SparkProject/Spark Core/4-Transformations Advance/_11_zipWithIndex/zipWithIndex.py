7348839ad76c497390e80db720825566ce1a17f8
3340165724 <3340165724@qq.com>
2023-05-26 08:56:58 +0800

    _

--- BigDataComponents/Spark/SparkProject/Spark Core/4-Transformations Advance/_11_zipWithIndex/zipWithIndex.py
+++ BigDataComponents/Spark/SparkProject/Spark Core/4-Transformations Advance/_11_zipWithIndex/zipWithIndex.py
@@ -14,7 +14,7 @@ sc = spark.sparkContext
 rdd1 = sc.parallelize([20, 30, 40, 50, 60], 3)
 rdd2 = sc.parallelize(["zhangsan", "lisi", "wangwu", "sunfeng", "wulan"], 3)
 
-rdd = rdd2.zip(rdd1).foreach(print)
+rdd = rdd2.zip(rdd1).zipWithIndex().foreach(print)
 
 sc.stop()
 spark.stop
--- BigDataComponents/Spark/SparkProject/target/classes/_10_zip/zip_python.py
+++ BigDataComponents/Spark/SparkProject/target/classes/_10_zip/zip_python.py
@@ -11,20 +11,10 @@ spark = SparkSession \
     .getOrCreate()
 sc = spark.sparkContext
 
-rdd = sc.parallelize([20, 30, 40, 50, 60], 3)
+rdd1 = sc.parallelize([20, 30, 40, 50, 60], 3)
+rdd2 = sc.parallelize(["zhangsan", "lisi", "wangwu", "sunfeng", "wulan"], 3)
 
-# rdd.map(lambda index,element:"[value=%s, partition_id=%d]" % (element,index)).foreach(print)
-
-list = []
-
-
-def fun(index, element):
-    for x in element:
-        list.append(x)
-        yield "[value=%s, partition_id=%d]" % (x, index)
-
-
-rdd.mapPartitionsWithIndex(fun).foreach(print)
+rdd = rdd2.zip(rdd1).foreach(print)
 
 sc.stop()
 spark.stop
