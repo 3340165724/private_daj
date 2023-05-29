caf295c2412445bc26e7a1b1f4886af02e1d4454
3340165724 <3340165724@qq.com>
2023-05-29 23:35:44 +0800

    _

---
+++ BigDataComponents/Spark/SparkProject/Spark Core/4-Transformations Advance/_17_cartesian/CartesianScala.scala
@@ -0,0 +1,25 @@
+package _17_cartesian
+
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+
+object CartesianScala {
+  def main(args: Array[String]): Unit = {
+    val spark: SparkSession = SparkSession
+      .builder()
+      .master("local[1]")
+      .appName("spark")
+      .getOrCreate()
+    val sc: SparkContext = spark.sparkContext
+
+
+    val  rdd1 = sc.parallelize(1 to 3)
+    val rdd2 = sc.parallelize(8 to 10)
+
+    println(rdd1.cartesian(rdd2).collect().mkString(","))
+    println(rdd2.cartesian(rdd1).collect().mkString(","))
+
+    sc.stop()
+    spark.stop()
+  }
+}
