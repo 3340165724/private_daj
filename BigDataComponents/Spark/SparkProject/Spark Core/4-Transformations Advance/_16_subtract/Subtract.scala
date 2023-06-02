e5e233f0927306c225d0366b47cf7dd6c08c16c8
3340165724 <3340165724@qq.com>
2023-06-02 22:07:57 +0800

    _

--- BigDataComponents/Spark/SparkProject/Spark Core/4-Transformations Advance/_16_subtract/Subtract.scala
+++ BigDataComponents/Spark/SparkProject/Spark Core/4-Transformations Advance/_16_subtract/Subtract.scala
@@ -1,9 +1,9 @@
-package _17_cartesian
+package _16_subtract
 
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
 
-object CartesianScala {
+object Subtract {
   def main(args: Array[String]): Unit = {
     val spark: SparkSession = SparkSession
       .builder()
@@ -12,12 +12,11 @@ object CartesianScala {
       .getOrCreate()
     val sc: SparkContext = spark.sparkContext
 
+    val  rdd1 = sc.parallelize(1 to 10)
+    val rdd2 = sc.parallelize(8 to 20)
 
-    val  rdd1 = sc.parallelize(1 to 3)
-    val rdd2 = sc.parallelize(8 to 10)
-
-    println(rdd1.cartesian(rdd2).collect().mkString(","))
-    println(rdd2.cartesian(rdd1).collect().mkString(","))
+    println(rdd1.subtract(rdd2).collect().mkString(", "))
+    println(rdd2 .subtract(rdd1).collect().mkString(", "))
 
     sc.stop()
     spark.stop()
--- BigDataComponents/Spark/SparkProject/Spark Core/4-Transformations Advance/_20_keyBy/KeyBy.scala
+++ BigDataComponents/Spark/SparkProject/Spark Core/4-Transformations Advance/_20_keyBy/KeyBy.scala
@@ -1,9 +1,9 @@
-package _16_subtract
+package _20_keyBy
 
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
 
-object SubtractScala {
+object KeyBy {
   def main(args: Array[String]): Unit = {
     val spark: SparkSession = SparkSession
       .builder()
@@ -12,11 +12,6 @@ object SubtractScala {
       .getOrCreate()
     val sc: SparkContext = spark.sparkContext
 
-    val  rdd1 = sc.parallelize(1 to 10)
-    val rdd2 = sc.parallelize(8 to 20)
-
-    println(rdd1.subtract(rdd2).collect().mkString(", "))
-    println(rdd2 .subtract(rdd1).collect().mkString(", "))
 
     sc.stop()
     spark.stop()
