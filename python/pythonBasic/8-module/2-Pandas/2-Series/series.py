958648bc5a15ee49ed173efa1830ad2b7e722ae2
3340165724 <3340165724@qq.com>
2023-07-11 00:34:09 +0800

    _

--- python/pythonBasic/8-module/2-Pandas/2-Series/series.py
+++ python/pythonBasic/8-module/2-Pandas/2-Series/series.py
@@ -1,11 +1,12 @@
 import pandas
 
-s = pandas.Series(["zhangsan",25])
+s = pandas.Series(["zhangsan", 25])
 print(s)
 
-
-s1 = pandas.Series(["zhangsan",25],index=["name","age"])
+s1 = pandas.Series(["zhangsan", 25], index=["name", "age"])
 print(s1)
 
+# todo 默认情况 read_csv 函数读取逗号分隔的文件，sep='\t' 显示指明使用制表符分隔
+s2 = pandas.read_csv(r'../data/gapminder.tsv', sep='\t')
 
-
+print(s2.shape)
