9aa07ef37abe3000b8c11f2100746b68a5ca9973
3340165724 <3340165724@qq.com>
2023-08-19 22:41:21 +0800

    _

--- BigDataCompetition/NationalCompetitionQuestions/2022_shtd_store/src/main/examination2/shtd_store.sql
+++ BigDataCompetition/NationalCompetitionQuestions/2022_shtd_store/src/main/examination2/shtd_store.sql
@@ -3,18 +3,14 @@
 use shtd_store;
 
 # 1、根据dwd层表统计每人每天下单的数量和下单的总金额，
-#    存入dws层的customer_consumption_day_aggr表（表结构如下）中，
-#    然后在hive cli中按照cust_key，totalconsumption, totalorder三列均逆序排序的方式，查询出前5条，
-#    将SQL语句与执行结果截图粘贴至对应报告中;
-#        字段	    类型	 中文含义	备注
+#   存入dws层的customer_consumption_day_aggr表（表结构如下）中，
+#   然后在hive cli中按照cust_key，totalconsumption, totalorder三列均逆序排序的方式，查询出前5条，
+#   将SQL语句与执行结果截图粘贴至对应报告中;
+#       字段	类型	中文含义	备注
 #       cust_key	int	客户key
 #       cust_name	string	客户名称
 #       totalconsumption	double	消费总额	当月消费订单总额
-#       totalorder	int	订单总数	当月订单总额select c.CUSTKEY, c.NAME, sum(o.TOTALPRICE), count(o.TOTALPRICE),
-#        year(o.ORDERDATE), month(o.ORDERDATE), day(o.ORDERDATE)
-# from customer as c
-# inner join orders o on c.CUSTKEY=o.CUSTKEY
-# group by c.CUSTKEY, c.NAME, year(o.ORDERDATE), month(o.ORDERDATE), day(o.ORDERDATE)
+#       totalorder	int	订单总数	当月订单总额
 #       year	int	年	订单产生的年
 #       month	int	月	订单产生的月
 #       day	int	日	订单产生的日
