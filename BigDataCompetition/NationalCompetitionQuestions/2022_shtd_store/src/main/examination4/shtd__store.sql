724231615f1f2ddc140f06fcff0318df98ee240d
3340165724 <3340165724@qq.com>
2023-08-21 16:01:53 +0800

    _

--- BigDataCompetition/NationalCompetitionQuestions/2022_shtd_store/src/main/examination4/shtd__store.sql
+++ BigDataCompetition/NationalCompetitionQuestions/2022_shtd_store/src/main/examination4/shtd__store.sql
@@ -10,8 +10,19 @@
 #       year	int	年	订单产生的年
 #       month	int	月	订单产生的月
 #       day	int	日	订单产生的日
-
-
+-- 统计每人每天下单的数量和下单的总金额
+select c.CUSTKEY, c.NAME, sum(o.TOTALPRICE), count(*), year(o.ORDERDATE), month(o.ORDERDATE),day(o.ORDERDATE)
+from customer as c
+inner join orders as o on c.CUSTKEY=o.CUSTKEY
+group by c.CUSTKEY, c.NAME, year(o.ORDERDATE), month(o.ORDERDATE),day(o.ORDERDATE)
+-- 存入dws层的customer_consumption_day_aggr表（表结构如下）中，然后在hive cli中按照cust_key，totalconsumption, totalorder三列均逆序排序的方式，查询出前5条
+select customer_consumption_day_aggr.*
+from (select c.CUSTKEY as ck, c.NAME, sum(o.TOTALPRICE) as s1, count(*) as c1, year(o.ORDERDATE), month(o.ORDERDATE), day(o.ORDERDATE)
+      from customer as c
+      inner join orders as o on c.CUSTKEY=o.CUSTKEY
+      group by c.CUSTKEY, c.NAME, year(o.ORDERDATE), month(o.ORDERDATE),day(o.ORDERDATE)) as customer_consumption_day_aggr
+order by customer_consumption_day_aggr.ck desc, customer_consumption_day_aggr.s1 desc, customer_consumption_day_aggr.c1 desc
+limit 5;
 
 
 # 2、根据dws层表customer_consumption_day_aggr表，
@@ -32,8 +43,28 @@
 #       year	int	年	订单产生的年
 #       month	int	月	订单产生的月
 #       sequence	Int	次序
+-- 每人每个月下单的数量和下单的总金额
+select c.CUSTKEY, c.NAME as cn, n.NATIONKEY, n.NAME as nn, r.REGIONKEY, r.NAME as rn,
+       sum(o.TOTALPRICE) as s1, count(*) as c1,
+       year(o.ORDERDATE) as y1, month(o.ORDERDATE) as m1,
+       row_number() over (order by sum(o.TOTALPRICE) desc, count(*) desc, month(o.ORDERDATE) desc)
+from customer as c
+inner join orders as o on c.CUSTKEY=o.CUSTKEY
+inner join nation as n on n.NATIONKEY=c.NATIONKEY
+inner join  region as r on r.REGIONKEY=n.REGIONKEY
+group by c.CUSTKEY, c.NAME, n.NATIONKEY, n.NAME, r.REGIONKEY, r.NAME,  y1, m1
+order by c.CUSTKEY, c1 desc, s1 desc, m1 desc;
 
-
+# 方法二：
+select t1.*, row_number() over (order by  s1 desc, c1 desc, m1 desc)
+from (select c.CUSTKEY, c.NAME as cn, n.NATIONKEY, n.NAME as nn, r.REGIONKEY, r.NAME as rn,
+             sum(o.TOTALPRICE) as s1, count(*) as c1,
+             year(o.ORDERDATE), month(o.ORDERDATE) as m1
+      from customer as c
+      inner join orders as o on c.CUSTKEY=o.CUSTKEY
+      inner join nation as n on n.NATIONKEY=c.NATIONKEY
+      inner join  region as r on r.REGIONKEY=n.REGIONKEY
+      group by c.CUSTKEY, c.NAME, n.NATIONKEY, n.NAME, r.REGIONKEY, r.NAME, year(o.ORDERDATE),month(o.ORDERDATE)) as t1
 
 
 
@@ -48,6 +79,16 @@
 #       nationname	text	国家名称
 #       nationmedianconsumption	double	该国家内客单价	该国家已购买产品的金额的中位数
 #       allnationmedianconsumption	double	所有国家订单的中位数	所有国家已购买的产品的金额的中位数
+-- 某年每个国家的消费额中中位数
+select n.NATIONKEY, n.NAME, sum(o.TOTALPRICE) as s1,
+from nation as n
+inner join customer as c on n.NATIONKEY=c.CUSTKEY
+inner join orders as o on c.CUSTKEY=o.CUSTKEY
+where year(o.ORDERDATE)=1992
+group by n.NATIONKEY, n.NAME
+
+
+
 
 
 
