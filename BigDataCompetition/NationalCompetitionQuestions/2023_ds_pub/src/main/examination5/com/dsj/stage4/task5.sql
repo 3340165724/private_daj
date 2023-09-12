d2b8493c37dfa9dc742024d1058b05af181e43e7
3340165724 <3340165724@qq.com>
2023-09-12 19:17:31 +0800

    _

--- BigDataCompetition/NationalCompetitionQuestions/2023_ds_pub/src/main/examination5/com/dsj/stage4/task5.sql
+++ BigDataCompetition/NationalCompetitionQuestions/2023_ds_pub/src/main/examination5/com/dsj/stage4/task5.sql
@@ -1,5 +1,5 @@
 # 进入数据库
-use ds_pub
+use ds_pub;
 
 
 # 2、根据dwd层表统计每个省份、每个地区、每个月下单的数量和下单的总金额，
@@ -16,7 +16,15 @@ use ds_pub
 #       totalorder	int	订单总数	当月订单总数
 #       year	int	年	订单产生的年
 #       month	int	月	订单产生的月
-#
+# 统计每个省份、每个地区、每个月下单的数量和下单的总金额
+select bp.id, bp.name, br.id, br.region_name, year(oi.create_time), month(oi.create_time), count(*), sum(oi.final_total_amount)
+from base_province as bp
+inner join base_region as br on bp.region_id = br.id
+inner join order_info as oi on bp.id = oi.province_id
+group by bp.id, bp.name, br.id, br.region_name, year(oi.create_time), month(oi.create_time)
+
+
+
 # 3、请根据dwd层表计算出2020年4月每个省份的平均订单金额和所有省份平均订单金额相比较结果（“高/低/相同”）,
 #   存入MySQL数据库shtd_result的provinceavgcmp表（表结构如下）中，
 #   然后在Linux的MySQL命令行中根据省份表主键、该省平均订单金额均为降序排序，查询出前5条，
@@ -28,8 +36,6 @@ use ds_pub
 #        provinceavgconsumption	double	该省平均订单金额
 #        allprovinceavgconsumption	double	所有省平均订单金额
 #        comparison	text	比较结果	该省平均订单金额和所有省平均订单金额比较结果，值为：高/低/相同
-
-
 
 
 # 4、根据dwd层表统计在两天内连续下单并且下单金额保持增长的用户，
